ac55ddba4130c951bba0564b4205fc7df4c304c5
Baptiste BOULLAY <baptiste-externe.boullay@edf.fr>
2021-10-13 15:15:13 +0200

    ajout d'un formulaire sur la page Localization

--- cra/src/pages/Localization.js
+++ cra/src/pages/Localization.js
@@ -1,4 +1,5 @@
 import { IntlProvider, FormattedMessage } from 'react-intl';
+import { useEffect, useState } from 'react';
 import Fr from '../translations/fr.json';
 import En from '../translations/en.json';
 
@@ -8,21 +9,51 @@ const translationEnum = {
     "en": En
 };
 
-export const Localization = () => <IntlProvider
-    messages={translationEnum[locale]}
-    locale={locale}
-    defaultLocale="en"
->
-    <h1>Localization</h1>
-    <FormattedMessage
-        id="myMessage"
-        defaultMessage="Today is {ts, date, :::yyyyMMdd}."
-        values={{ ts: Date.now() }}
-    />
-    <br/>
-    <FormattedMessage
-        id="myMessage2"
-        defaultMessage="Yesterday, I ate an apple."
-    />
+export const Localization = () => {
+    const [currentValue, setCurrentValue] = useState({
+        nom: localStorage.getItem("nom") ? localStorage.getItem("nom") : 'XXXX',
+        prenom: localStorage.getItem("prenom") ? localStorage.getItem("prenom") : 'XXXX'
+    });
 
-</IntlProvider>;
+    const onFormSubmit = form => {
+        form.preventDefault();
+        const formData = new FormData(form.target);
+        localStorage.setItem('nom', formData.get("nom"));
+        localStorage.setItem('prenom', formData.get("prenom"));
+        setCurrentValue({
+            nom: localStorage.getItem('nom'),
+            prenom: localStorage.getItem('prenom')
+        });
+    };
+
+    const onTextChange = (input) => {
+        setCurrentValue(input.target.value);
+    };
+
+    return <IntlProvider
+        messages={translationEnum[locale]}
+        locale={locale}
+        defaultLocale="en"
+    >
+        <h1>Localization</h1>
+        <FormattedMessage
+            id="myMessage"
+            defaultMessage="Today is {ts, date, :::yyyyMMdd}."
+            values={{ ts: Date.now() }}
+        />
+        <br />
+        <FormattedMessage
+            id="myMessage2"
+            defaultMessage="Yesterday, I ate an apple."
+        />
+
+        <h4>{currentValue.nom} {currentValue.prenom}</h4>
+
+        <form onSubmit={onFormSubmit}>
+            <input type="text" name="nom" className="form-control" onChange={onTextChange}></input>
+            <input type="text" name="prenom" className="form-control"></input>
+            <button className="btn btn-warning">Submit</button>
+        </form>
+
+    </IntlProvider>;
+}
